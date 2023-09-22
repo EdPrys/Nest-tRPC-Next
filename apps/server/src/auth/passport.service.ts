8cabe77e3e34ab6583996a3eccf619bf4dbd72bb
GetApple <user@users-MacBook-Air.local>
2023-09-22 14:19:11 +0300

    clean up

--- apps/server/src/auth/passport.service.ts
+++ apps/server/src/auth/passport.service.ts
@@ -1,5 +1,6 @@
 // auth/passport.service.ts
 import { Injectable } from '@nestjs/common';
+import { UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
